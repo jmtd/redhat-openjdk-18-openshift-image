2d861ed980ddf0e1ad5289d2def63884af1574a6
Jonathan Dowland <jdowland@redhat.com>
2018-11-05 10:57:20 +0000

    Add selinux stuff to test harness
    Signed-off-by: Jonathan Dowland <jdowland@redhat.com>

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ echo using uid $uid
 docker run --rm -ti \
     -u $uid \
     -v $(pwd)/testcert/ca/ca.cert.pem:/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt \
-    -v $(pwd)/testcert:/tmp/testcert \
-    -v $(pwd)/runme.sh:/tmp/runme.sh \
+    -v $(pwd)/testcert:/tmp/testcert:Z \
+    -v $(pwd)/runme.sh:/tmp/runme.sh:Z \
     redhat-openjdk-18/openjdk18-openshift:1.6 \
     /tmp/runme.sh
